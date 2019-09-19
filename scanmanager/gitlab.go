41900a007324a9ab9e2f5d168e3145cc42298f73
kodmi <kodmi@skbkontur.ru>
2019-09-19 14:16:54 +0500

    fix null map; add some logging

--- scanmanager/gitlab.go
+++ scanmanager/gitlab.go
@@ -1,26 +1,31 @@
 package scanmanager
 
 import (
+	"strconv"
+
 	"github.com/AlexAkulov/hungryfox"
 	"github.com/AlexAkulov/hungryfox/config"
 	"github.com/AlexAkulov/hungryfox/gitlab"
 )
 
 func (sm *ScanManager) inspectGitlab(inspect config.Inspect) error {
+	sm.Log.Debug().Str("status", "start").Msg("fetching gitlab projects")
 	gitlabClient := gitlab.Client{
 		Token:   inspect.Token,
 		URL:     inspect.GitlabURL,
 		WorkDir: inspect.WorkDir,
 	}
-	var repoLocations map[hungryfox.RepoLocation]struct{}
+	repoLocations := make(map[hungryfox.RepoLocation]struct{})
 
 	fetchOpts := &gitlab.FetchOptions{
-		ExcludeNamespaces: &inspect.ExcludeNamespaces,
+		ExcludeNamespaces: inspect.ExcludeNamespaces,
 	}
 	locations, err := gitlabClient.FetchGroupRepos(fetchOpts)
 	if err != nil {
+		sm.Log.Error().Str("status", "err").Msg(err.Error())
 		return err
 	}
+	sm.Log.Debug().Str("status", "complete").Str("count", strconv.Itoa(len(locations))).Msg("fetching gitlab projects")
 	for _, location := range locations {
 		repoLocations[location] = struct{}{}
 	}
